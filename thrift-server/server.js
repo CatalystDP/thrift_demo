366bf504c2f9a07843c55e862fff4aaf2e47b3e4
CatalystDP <469589860@qq.com>
2016-09-15 00:33:05 +0800

    server.js

--- thrift-server/server.js
+++ thrift-server/server.js
@@ -1,2 +1,14 @@
 'use strict';
 const thrift = require('thrift');
+const HelloMethod=require('../gen-nodejs/HelloMethod');
+const hello_type=require('../gen-nodejs/hello_types');
+
+let HelloMethodImpl={
+    sayHello:function(helloMsg,callback){
+        console.log('say hello from ',helloMsg.name);
+        callback(null);
+    }
+};//HelloMethod接口实现
+
+let server=thrift.createServer(HelloMethod,HelloMethodImpl);
+server.listen(10000);
